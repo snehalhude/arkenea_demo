b1eac6366acd3a5d3626d5b0ce95742e387a14f1
Snehal Hude <snehalh1793@gmail.com>
2020-09-14 15:18:47 +0530

    change name

--- application/views/common/nav.php
+++ application/views/common/nav.php
@@ -11,7 +11,7 @@
     <a href="index3.html" class="brand-link">
       <img src="<?= base_url() ?>assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light">VW Demo</span>
+      <span class="brand-text font-weight-light">Arkenea Demo</span>
     </a>
 
     <!-- Sidebar -->
--- application/views/common/start_header.php
+++ application/views/common/start_header.php
@@ -9,7 +9,7 @@ if(isset($_SESSION['id']) != ''){
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 3 | Log in</title>
+  <title>Arkenea Demo | Login</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
